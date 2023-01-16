81cc887dbecac55697100dd5fef01c4e8b850845
Boris <70385493+bbostandzhiev-qb@users.noreply.github.com>
2023-01-17 01:30:48 +0200

    Tweaked global styles

--- outdoorsy-assessment/src/pages/_app.tsx
+++ outdoorsy-assessment/src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import { AppProps } from 'next/app'
 
+const PageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const FakeMobileView = styled.div`
+  width: 30%;
+`
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PageWrapper>
+      <FakeMobileView>
+        <Component {...pageProps} />
+      </FakeMobileView>
+    </PageWrapper>
+  )
 }
 
 export default MyApp
