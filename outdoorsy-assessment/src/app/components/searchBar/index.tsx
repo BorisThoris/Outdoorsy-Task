538782f72ea35719965a8a3e893123d8b0ae4295
Boris <70385493+bbostandzhiev-qb@users.noreply.github.com>
2023-01-18 00:00:49 +0200

    Update index.tsx

--- outdoorsy-assessment/src/app/components/searchBar/index.tsx
+++ outdoorsy-assessment/src/app/components/searchBar/index.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import styled from 'styled-components'
 import _ from 'lodash'
 
 const SearchInput = styled.input`
   width: 100%;
   height: 40px;
-
   padding-left: 10px;
   border-radius: 8px;
-
   border: 1.9px double darkgray;
 `
 
@@ -17,11 +15,25 @@ interface SearchBarProps {
 }
 
 export const SearchBar = ({ refetchRentals }: SearchBarProps) => {
-  const debouncedRefetch = _.debounce((val) => refetchRentals(val), 1500)
+  const [search, setSearch] = useState('')
+  const debouncedRefetch = useCallback(
+    _.debounce((val) => refetchRentals(val), 1500),
+    [refetchRentals],
+  )
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    debouncedRefetch(e.target.value.toLowerCase())
-  }
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value)
+      debouncedRefetch(e.target.value.toLowerCase())
+    },
+    [debouncedRefetch],
+  )
 
-  return <SearchInput data-test-id="search-input" onChange={handleChange} />
+  return (
+    <SearchInput
+      data-test-id="search-input"
+      onChange={handleChange}
+      value={search}
+    />
+  )
 }
