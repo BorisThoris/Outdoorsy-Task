9505962cbee07ad9b0c216d868dd087d257eb8d4
Boris <70385493+bbostandzhiev-qb@users.noreply.github.com>
2023-01-17 04:28:18 +0200

    Fixed unit tests failing due to Next forcing jsx preserve

--- jest.confing.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts', '**/*.test.tsx'],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-      '^.+\\.ts?$': 'ts-jest',
-    },
-  };
